0e9f1a6b1a5668710ef9a77c7b86c0ba9502286a
nickg <nick.gilliosipescu@gmail.com>
2019-01-12 22:44:02 -0400

    initial commit -- tabbed view with spinner

---
+++ app/src/main/java/com/example/nickg/koincompare/TabOne.java
@@ -0,0 +1,33 @@
+package com.example.nickg.koincompare;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class TabOne extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View  view = inflater.inflate(R.layout.tab_one, container, false);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.coin_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.coin_names, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+
+        return view;
+
+
+    }
+}
